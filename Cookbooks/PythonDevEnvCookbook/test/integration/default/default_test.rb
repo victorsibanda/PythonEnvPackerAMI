09be3e2c54873a211e1d085fce44f51c1e0641d7
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-16 11:14:43 +0100

    Default Test changed in cookbook

--- Cookbooks/PythonDevEnvCookbook/test/integration/default/default_test.rb
+++ Cookbooks/PythonDevEnvCookbook/test/integration/default/default_test.rb
@@ -8,67 +8,103 @@ describe port(80) do
   it { should_not be_listening }
 end
 
-describe file('/usr/local/lib/python3.6/dist-packages/atomicwrites')do
-  it {should exist}
+describe file('/usr/local/lib/python3.6/dist-packages/atomicwrites') do
+  it { should exist }
 end
-describe file('/usr/local/lib/python3.6/dist-packages/attrs')do
-  it {should exist}
- end
-# describe file('/usr/local/lib/python3.6/dist-packages/beautifulsoup4')do
-#   it {should exist}
-# end
-describe file('/usr/local/lib/python3.6/dist-packages/certifi')do
-  it {should exist}
+
+describe file('/usr/local/lib/python3.6/dist-packages/attrs-19.1.0.dist-info') do
+  it { should exist }
 end
-# describe file('/usr/local/lib/python3.6/dist-packages/chardet')do
-#   it {should exist}
-# end
-describe file('/usr/local/lib/python3.6/dist-packages/idna')do
-  it {should exist}
+
+describe file('/usr/local/lib/python3.6/dist-packages/bs4') do
+  it { should exist }
 end
-# describe file('/usr/local/lib/python3.6/dist-packages/importlib-metadata')do
-#   it {should exist}
-# end
-# describe file('/usr/local/lib/python3.6/dist-packages/more-itertools')do
-#   it {should exist}
-# end
-describe file('/usr/local/lib/python3.6/dist-packages/packaging')do
-  it {should exist}
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/certifi') do
+  it { should exist }
 end
-describe file('/usr/local/lib/python3.6/dist-packages/pluggy')do
-  it {should exist}
+
+describe file('/usr/lib/python3/dist-packages/chardet') do
+  it { should exist }
 end
-describe file('/usr/local/lib/python3.6/dist-packages/py')do
-  it {should exist}
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/idna') do
+  it { should exist }
 end
-# describe file('/usr/local/lib/python3.6/dist-packages/pyparsing')do
-#   it {should exist}
-# end
-# describe file('/usr/local/lib/python3.6/dist-packages/pytest')do
-#   it {should exist}
-# end
-describe file('/usr/local/lib/python3.6/dist-packages/requests')do
-  it {should exist}
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/importlib_metadata') do
+  it { should exist }
 end
-# describe file('/usr/local/lib/python3.6/dist-packages/six')do
-#   it {should exist}
-# end
-describe file('/usr/local/lib/python3.6/dist-packages/soupsieve')do
-  it {should exist}
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/more_itertools') do
+  it { should exist }
 end
-describe file('/usr/local/lib/python3.6/dist-packages/urllib3')do
-  it {should exist}
+
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/packaging') do
+  it { should exist }
 end
-describe file('/usr/local/lib/python3.6/dist-packages/wcwidth')do
-  it {should exist}
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/pluggy') do
+  it { should exist }
 end
-# describe file('/usr/local/lib/python3.6/dist-packages/zipp')do
-#   it {should exist}
-# end
+
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/py') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/pyparsing.py') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/pytest.py') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/requests') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/six.py') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/soupsieve') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/urllib3') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/wcwidth') do
+  it { should exist }
+end
+
+
+describe file('/usr/local/lib/python3.6/dist-packages/zipp.py') do
+  it { should exist}
+end
 
 describe file('/usr/local/lib/python3.6/dist-packages/bs4')do
   it {should exist}
 end
+
 describe package('python3') do
   it { should be_installed }
   its("version"){ should cmp > '3.*'}
